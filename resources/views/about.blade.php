54bb09bb6e0b5992880f3dcc26534c193d99a563
ilhamibnuu <e41210178@student.polije.ac.id>
2023-02-09 15:08:07 +0700

    update repo ngab

---
+++ resources/views/about.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<style>
+    body {
+        background-color: pink;
+    }
+</style>
+
+<body>
+    <h3>About</h3>
+    <a href="/">Home</a>
+    <h4>Prodi : Teknik Informatika</h4>
+    <h4>Nama : Ilham Ibnu Ahmad</h4>
+    <h4>Nim : E41210178</h4>
+    <h4>Juruasn : Teknologi Informasi</h4>
+    <img src="https://awsimages.detik.net.id/visual/2022/09/11/bjorka.jpeg?w=650" alt="" width="200">
+
+</body>
+
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<style>
+    body {
+        background-color: pink;
+    }
+</style>
+
+<body>
+    <h3>Home</h3>
+    <a href="/about">About</a>
+    <h4>Belajar Workshop Sistem Informasi Web Framework - Laravel</h4>
+
+</body>
+
+</html>
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset='UTF-8' />
+    <meta content='width=device-width, initial-scale=1, user-scalable=1, minimum-scale=1, maximum-scale=5' name='viewport' />
+    <meta content='IE=edge' http-equiv='X-UA-Compatible' />
+    <title>Jurusan Teknologi Informasi</title>
+    <link rel="icon" type="image/png" sizes="16x16" href="https://static.whatsapp.net/rsrc.php/v3/yz/r/ujTY9i_Jhs1.png" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta content="Kuisioner Jurusan Teknologi Informasi" name="description" />
+    <meta content="Politeknik Negeri Jember" name="author" />
+</head>
+
+<style>
+    @import url("https://fonts.googleapis.com/css?family=Roboto:400,500,700");
+
+    html {
+        position: relative;
+        min-height: 100%;
+        background-color: #f2f5f7;
+    }
+
+    body {
+        margin: 0;
+        overflow-x: hidden;
+        font-size: 14px;
+        font-family: "Roboto", sans-serif;
+    }
+
+    .background {
+        background: url("https://upload.wikimedia.org/wikipedia/commons/c/ce/Jurusan_Teknologi_Informasi_Politeknik_Jember.jpg");
+        background-size: cover;
+        background-position: center center;
+        height: 100%;
+        width: 100%;
+        top: 0;
+        -webkit-box-shadow: inset 0 0 0 2000px rgba(35, 34, 47, 0.8);
+        box-shadow: inset 0 0 0 2000px rgba(35, 34, 47, 0.8);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        justify-items: center;
+        align-content: center;
+
+    }
+
+    .vh-100 {
+
+        height: 100vh !important;
+    }
+
+    .marquee {
+        width: 500px;
+        height: auto;
+        margin-top: 10px;
+    }
+
+    * {
+        padding: 0px;
+        margin: 0;
+    }
+
+    .border {
+        border: 1px solid #dee2e6;
+    }
+
+    .border-primary {
+        border-color: #5766da;
+    }
+
+    .ml-auto,
+    .mx-auto {
+        margin-left: auto;
+    }
+
+    .mr-auto,
+    .mx-auto {
+        margin-right: auto;
+    }
+
+    .w-25 {
+        width: 25%;
+    }
+
+    .thumb-sm {
+        padding: 5px;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .img {
+        align-items: center;
+        justify-content: center;
+    }
+
+    .mt-3 {
+        padding: 5px;
+        color: white;
+        text-align: center;
+        justify-content: center;
+        align-items: center;
+        justify-items: center;
+    }
+
+    .account-title {
+        padding-top: 200px;
+        color: white;
+        text-align: center;
+        justify-content: center;
+        align-items: center;
+        justify-items: center;
+    }
+
+    .accountbg {
+        text-align: center;
+        justify-content: center;
+        align-items: center;
+        justify-items: center;
+    }
+
+    * h1 {
+        font-size: 60px;
+    }
+
+    * h4 {
+        font-size: 25px;
+    }
+
+    .col-lg-9 {
+        justify-content: center;
+        align-items: center;
+        align-content: center;
+        text-align: center;
+    }
+
+    .p-0 {
+        padding: 0 !important;
+
+    }
+</style>
+
+
+<body class="background">
+    <div class="row vh-100">
+        <div class="col-lg-9 p-0">
+            <div class="accountbg">
+                <div class="account-title">
+                    <img src="http://103.109.209.245/jtiform/assets/img/favicon.png" alt="" class="thumb-sm" />
+                    <h4 class="mt-3">Welcome To JTI Polije</h4>
+                    <div class="border w-25 mx-auto border-primary"></div>
+                    <h1 class="marquee">
+                        <marquee scrollamount="15">TETAP SEMANGAT NUGAS WALAUPUN GA DISEMANGATIN AYAANG</marquee>
+                    </h1>
+                    <p class="font-14 mt-3">
+                        #PolijeSIP #JTIPolije #TIPC
+                        <a href="" class="text-primary"></a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</body>
+
+</html> -->
